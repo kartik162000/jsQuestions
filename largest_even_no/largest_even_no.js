1c01b5e153f42f2de130512f3964af80dffeb99d
kartik162000 <kartikg.goel@gmail.com>
2023-01-16 01:40:23 +0530

    fix:fix the error

--- largest_even_no/largest_even_no.js
+++ largest_even_no/largest_even_no.js
@@ -1,27 +1,26 @@
 function largeEvenNoReg(nums)
 {
-    let max_element=Number.MIN_VALUE
-    let flag=0;
+    let max_element=-1
     nums.forEach(element => {
-     max_element=(element%2==0)?(Math.max(element,max_element), flag=1) :max_element
+     max_element=(element%2==0)?(Math.max(element,max_element)) :max_element
     })
-    // console.log(max_element);
-    return (flag==1)? max_element: -1;
+    return max_element;
 }
 
 const largeEvenNoArrow = (nums)=>
 {
-    let max_element=Number.MIN_VALUE
-    let flag=0;
+    let max_element=-1
     nums.forEach(element => {
-     max_element=(element%2==0)?(Math.max(element,max_element), flag=1) :max_element
+     max_element=(element%2==0)?(Math.max(element,max_element)) :max_element
     })
-    return (flag==1)? max_element: -1;
+    return max_element;
 }
 
 
 
-let nums=[1,3,5,7,9]
+let nums=[1,3,5,7,8,10]
 
 console.log(largeEvenNoReg(nums));
 console.log(largeEvenNoArrow(nums));
+// module.exports = {largeEvenNoReg, largeEvenNoArrow};
+
