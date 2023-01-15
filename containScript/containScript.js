006306bde3c95973981045b9ea4e29a08c6b9462
kartik162000 <kartikg.goel@gmail.com>
2023-01-16 01:15:41 +0530

    feat: add feature to find string has Script

---
+++ containScript/containScript.js
@@ -0,0 +1,16 @@
+function containScriptRegular(input) {
+    if(input.endsWith("Script") && input.length >= 6)
+    {
+        return true;
+    }
+    return false;
+}
+
+// Arrow function using ternary operators
+let containScriptArrow=(input) => {
+    let ans = (input.endsWith("Script")) ? (input.length >= 6) ? true : false : false;
+    return ans;
+}
+
+console.log(containScriptRegular('JavaScript'));
+console.log(containScriptArrow('javascript'));
