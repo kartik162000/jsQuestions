fb6ab07a8c53ea2bccfec01c73b001e2bd0fd3ab
kartik162000 <kartikg.goel@gmail.com>
2023-01-16 01:18:14 +0530

    feat: add feature to change the string to following alphabet

---
+++ nextAlpha/nextAlpha.js
@@ -0,0 +1,48 @@
+function nextAlphaRegular(input)
+{
+    let finalString = "";
+    for(let idx=0; idx<input.length; idx++)
+    {
+        let charCode = input.charCodeAt(idx) + 1;
+        if(charCode == 123)
+        {
+            finalString += 'a';
+            continue;
+        }
+        if(charCode == 91)
+        {
+            finalString += 'A';
+            continue;
+        }
+        let char = String.fromCharCode(charCode);
+        finalString += char;
+    }
+    return finalString;
+}
+
+const nextAlphaArrow=(input) =>
+{
+    let finalString = "";
+    for(let idx=0; idx<input.length; idx++)
+    {
+        let charCode = input.charCodeAt(idx) + 1;
+        if(charCode == 123)
+        {
+            finalString += 'a';
+            continue;
+        }
+        if(charCode == 91)
+        {
+            finalString += 'A';
+            continue;
+        }
+        let char = String.fromCharCode(charCode);
+        finalString += char;
+    }
+    return finalString;
+}
+
+
+let input="lazyinterns"
+console.log(nextAlphaRegular(input));
+console.log(nextAlphaArrow(input));
